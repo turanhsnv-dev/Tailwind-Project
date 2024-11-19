30b902aab96003fe8c73895649594fac0530221e
turanlz <turanhasan013@gmail.com>
2024-11-19 20:57:02 +0400

    Home Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        lg: "1140px",
+        xl: "1140px",
+        "2xl": "1140px",
+      },
+    },
+
+    extend: {
+      fontFamily: {
+        gemunu: ["Gemunu Libre", "sans-serif"],
+        open: ["Open Sans", "sans-serif"],
+      },
+
+      colors: {
+        "gega-red": "#3bc1a45",
+        "gega-melon": "#ffd369",
+        "gega-grey": "#dddddd",
+        "gega-white": "#f7f7f7",
+      },
+
+      spacing: {
+        128: "32rem",
+      },
+    },
   },
   plugins: [],
 };
